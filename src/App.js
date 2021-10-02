3df1a656d668a399830199b38c39e346ba20a8eb
Ann S. Abdelwahed <ann.s.eit@gmail.com>
2021-10-02 18:59:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import Footer from "./components/Footer";
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([]);
